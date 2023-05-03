fdde461886a695d2a05b124e5c4175c16511b1d3
sanderpalk <sanderpalk0@gmail.com>
2023-05-03 16:11:48 +0300

    Clients model schema and endpoints for trainer clients, login endpoint

---
+++ models/Client.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const {ObjectId} = require("mongodb");
+
+const ClientsSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    password: {type: String, required: true},
+    birthday: {type: Date},
+    gender: {type: String},
+    email: {type: String, required: true},
+    city: {type: String},
+    home_gym: {type: String},
+    phone: {type: Number},
+    trainerId: {type: ObjectId, ref: 'Trainers'}
+})
+
+const TrainerModel = mongoose.model("Clients", ClientsSchema)
+
+module.exports = TrainerModel
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
 const TrainerModel = require("./models/Trainer")
+const ClientModel = require("./models/Client")
 
 const corsOptions = {
     origin: ['http://localhost:3000', process.env.APP_URI]
@@ -24,6 +25,44 @@ app.listen(port, () => {
     console.log(`Server Listening on port ${port}`)
 })
 
+async function saveTrainer() {
+    try {
+        const trainer = new TrainerModel({
+            name: "Admin",
+            password: "qwerty123",
+            birthday: new Date("2000-11-03"),
+            gender: "Male",
+            email: "admin@fitnet.ee",
+            city: "Tartu",
+            home_gym: "Tartu GYM!",
+            phone: "595959555"
+        })
+        console.log(trainer)
+        await trainer.save()
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
+async function saveClient() {
+    try {
+        const client = new ClientModel({
+            name: 'Arnold',
+            password: 'password12311',
+            birthday: new Date("1947-06-30"),
+            gender: "Male",
+            email: "arnold@client.fit",
+            city: "Berlin",
+            home_gym: "Sparta",
+            phone: "1337",
+            trainerId: '644faf4484fe1969b0782942'
+        })
+        await client.save()
+    } catch (e) {
+        console.log(e.message)
+    }
+}
+
 app.get('/trainers/:id', async (req, res) => {
     try {
         const id = req.params.id;
@@ -38,45 +77,71 @@ app.get('/trainers/:id', async (req, res) => {
     }
 })
 
-async function saveTrainer() {
-    try {
-        const trainer = new TrainerModel({
-            name: "Sander",
-            password: "qwerty123",
-            birthday: new Date("2000-11-03"),
-            gender: "Male",
-            email: "sander.palk@voco.ee",
-            city: "Tartu",
-            home_gym: "Tartu GYM!",
-            phone: "595959555"
-        })
-        console.log(trainer)
-        await trainer.save()
-    } catch (e) {
-        console.log(e.message)
-    }
-}
-
 app.put('/trainers/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const updatedFields = {
-             name: req.body.name,
-             birthday: req.body.birthday,
-             email: req.body.email,
-             phone: req.body.phone,
-             gender: req.body.gender,
-             city: req.body.city,
-             home_gym: req.body.home_gym
-         };
-         Object.keys(updatedFields).forEach(key => {
-             if (!updatedFields[key]) {
-                 delete updatedFields[key];
-             }
-         });
+            name: req.body.name,
+            birthday: req.body.birthday,
+            email: req.body.email,
+            phone: req.body.phone,
+            gender: req.body.gender,
+            city: req.body.city,
+            home_gym: req.body.home_gym
+        };
+        Object.keys(updatedFields).forEach(key => {
+            if (!updatedFields[key]) {
+                delete updatedFields[key];
+            }
+        });
         await TrainerModel.findOneAndUpdate({_id: id}, updatedFields, {new: true});
     } catch (error) {
         console.log(error);
         return res.status(500).send('Internal Server Error');
     }
 });
+
+function validatePassword(thePassword, validatingPassword) {
+    if (thePassword === validatingPassword) {
+        return true;
+    }
+}
+
+app.post('/login', async (req, res) => {
+    try {
+        const trainer = await TrainerModel.findOne({email: req.body.email})
+        if (trainer) {
+            const userPassword = trainer.password
+            const requestPassword = req.body.password
+            if (validatePassword(userPassword, requestPassword)) {
+                return res.status(202).send({id: trainer._id})
+            } else {
+                return res.status(406).send({error: 'Wrong password'})
+            }
+        } else {
+            return res.status(406).send({error: 'User not found'})
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send('Internal Server Error')
+    }
+})
+
+app.get('/trainer/clients/:id', async (req, res) => {
+    try {
+        const trainerId = req.params.id
+        const clients = await ClientModel.find({ trainerId: trainerId})
+        if (clients) {
+            return res.status(200).send(clients)
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('Internal Server Error')
+    }
+})
+
+// app.post('/register', async (req, res) => {
+//     try {
+//
+//     }
+// })
